5a4998cfc38a62539432281de5db41a8321390e0
JustinCasler <97986810+JustinCasler@users.noreply.github.com>
2023-10-28 16:59:50 -0400

    Add login logic and frontend (issue #3)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import SongOverview from './components/SongOverview';
 import CreateUser from './components/CreateUser';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Song OverView</Link>
-          </li>
-          <li>
-            <Link to="user/create">Create User </Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route index element={<SongOverview />} />
-        <Route path="user/create" element={<CreateUser />} />
-      </Routes>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Song OverView</Link>
+            </li>
+            <li>
+              <Link to="user/create">Create User</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<SongOverview />} />
+          <Route path="user/create" element={<CreateUser />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,64 +1,111 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+
 export default function CreateUser() {
-    const [inputs, setInputs] = useState({})
+  const [inputs, setInputs] = useState({});
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState("");
 
-    const handleChange = (event) => {
-        const name = event.target.name 
-        const value = event.target.value
-        setInputs(values => ({...values, [name]: value}))
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (Object.keys(inputs).length < 3) {
+      setError("Please dont leave any field blank");
+    } else if(inputs.password !== inputs.confirm) {
+      setError("Password and Confirm Password must match.");
+    } else if (inputs.password.length < 10) {
+      setError("Password must be at least 10 characters long.");
+    } else {
+      // Reset error state
+      setError(null);
+
+      // Perform your axios post request here
+      axios.post("http://localhost/backend/index.php", { inputs, action: "register" })
+        .then((response) => {
+          if (response.data.status === 1) {
+            // Registration was successful
+            setMessage("Registration successful.");
+          } else {
+            // Registration failed, display the error message from the backend
+            setMessage(response.data.message);
+          }
+        })
+        .catch((error) => {
+          setMessage("An error occurred while processing your request.");
+        });
     }
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  };
 
-        axios.post('http://localhost/backend/user/save', inputs)
-        console.log(inputs)
+  const handleClear = () => {
+    // Clear form inputs and reset error state
+    setInputs({});
+    setError(null);
+  };
 
-        function validateForm() {
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
-            var confirmPassword = document.getElementById("confirmPassword").value;
-            console.log(username)
-            if (username == null || username.length ==0){
-                alert("Username cannot be blank");
-                // Prevent form submission
-                return false; 
-            }
-            if (password !== confirmPassword) {
-                alert("Passwords do not match!");
-                // Prevent form submission
-                return false; 
-            }
-            if (password.length < 10) {
-                alert("Password must be at least 10 characters long!");
-                // Prevent form submission
-                return false; 
-            }
-            // Allow form submission
-            return true; 
-        }
-        function clearForm() {
-            document.getElementById("username").value = "";
-            document.getElementById("password").value = "";
-            document.getElementById("confirmPassword").value = "";
-            // You can add more input fields to clear here
-        }
-    }
-    return (
-        <div>
-            <h1>Create User</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Username: </label>
-                <input type="text" name="username" onChange={handleChange}/>
-                <br />
-                <label>Password: </label>
-                <input type="password" name="password" onChange={handleChange}/>
-                <br />
-                <label>Confirm Password: </label>
-                <input type="password" name="confirm" onChange={handleChange}/>
-                <br />
-                <button>Submit</button>
-            </form>
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+            <h1 className="card-title text-center">Sign Up for PlaylistPulse</h1>
+              {error && <p className="text-danger text-center">{error}</p>}
+              {message && <p className="text-danger text-center">{message}</p>}
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="username" className="form-label">Username:</label>
+                  <input
+                    type="text"
+                    id="username"
+                    className="form-control"
+                    name="username"
+                    value={inputs.username || ""}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    name="password"
+                    value={inputs.password || ""}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="confirm" className="form-label">Confirm Password:</label>
+                  <input
+                    type="password"
+                    id="confirm"
+                    className="form-control"
+                    name="confirm"
+                    value={inputs.confirm || ""}
+                    onChange={handleChange}
+                  />
+                </div>
+                <button type="submit" className="mr-3 btn btn-primary" name="submit">
+                Submit
+                </button>
+                <button type="button" className="ml-3 btn btn-secondary" name="clear" onClick={handleClear}>
+                Clear
+                </button>
+                <p>
+                  Already have an account? <Link to="/">Login here</Link>
+                </p>
+              </form>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
