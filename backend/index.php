ac61a3a619bab42104bec5620c0efa80f60d9b45
JustinCasler <97986810+JustinCasler@users.noreply.github.com>
2023-10-29 14:50:12 -0400

    let all access through for local dev

--- backend/index.php
+++ backend/index.php
@@ -3,6 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE");
+header("Access-Control-Allow-Credentials: true");
 
 include 'includes/DbConnect.php';
 
