ff5828f5de33c555034f37344c4f69930c53ebe6
Binh Do <binhdo@Binhs-MacBook-Air.local>
2023-10-29 18:48:47 -0400

    Add New Rating Logic (issue #6)

--- frontend/src/components/SongOverview.js
+++ frontend/src/components/SongOverview.js
@@ -1,5 +1,14 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export default function SongOverview() {
     return (
-        <h1>Song Overview</h1>
-    )
+        <div>
+            <h1>Song Overview</h1>
+            <Link to="/user/newrating" className="btn btn-secondary ms-2">
+                Add new Song Rating
+            </Link>
+        </div>
+    );
 }
